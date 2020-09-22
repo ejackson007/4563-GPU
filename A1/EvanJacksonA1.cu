48e789c24f46510cdadda264488f91f95b17687f
Evan Jackson <evanojackson@gmail.com>
2020-09-21 21:49:13 -0500

    waiting on output

---
+++ A1/EvanJacksonA1.cu
@@ -0,0 +1,56 @@
+//Evan Jackson
+#include <stdio.h>
+
+#define SIZE 4096
+
+int total = 0;
+
+__global__
+void vecAdd(int* aDevice, int* bDevice, int* cDevice){
+    int i = threadIdx.x + blockDim.x * blockId.x; // get location of thread
+    if(i < SIZE){
+        cDevice[i] = aDevice[i] + bDevice[i];//add to c and save to total
+        total += cDevice[i];
+    }
+}
+
+//host code
+int main(){
+    int arraySize = SIZE * sizeof(int);
+    int* aHost, bHost, cHost, aDevice, bDevice, cDevice;
+
+    //allocate for host and store
+    aHost = (int*)malloc(arraySize);
+    bHost = (int*)malloc(arraySize);
+    //fill array aHost
+    for(int i = 0; i < SIZE; i++){
+        aHost[i] = i;
+    }
+    //fill array bHost
+    for(int i = 0; i < SIZE; i++){
+        bHost[i] = 4905 + i;
+    }
+
+    //allocate memory for device and transfer to device
+    cudaMalloc((void**) &aDevice, arraySize);
+    cudaMalloc((void**) &bDevice, arraySize);
+    cudaMemcpy(aDevice, aHost, arraySize, cudaMemcpyHostToDevice);
+    cudaMemcpy(bDevice, bHost, arraySize, cudaMemcpyHostToDevice);
+
+    dim3 dimGrid(4,1,1); //1D of 4 block, so that each block will have maximum threads
+    dim3 dimBlock(1024,1,1);
+
+    //call gpu process
+    vecAdd<<<dimGrid,dimBlock>>>(aDevice, bDevice, cDevice);
+
+    //transfer back to host
+    cudaMemcpy(cHost, cDevice, arraySize, cudaMemcpyDeviceToHost);
+    //free device memory;96
+    cudaFree(aDevice);
+    cudaFree(bDevice);
+    cudaFree(cDevice);
+    //print values
+    printf("First element of vector C: %i\n", cHost[0]);
+    printf("Last element of vector C: %i\n", cHost[SIZE - 1]);
+    printf("Summation of Elements in vector C: %i\n", total);
+}
